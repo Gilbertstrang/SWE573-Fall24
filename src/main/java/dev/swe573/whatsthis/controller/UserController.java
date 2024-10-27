506db77d3d92c0cc39705de1adb0245e9c7be549
Enes Duman <ensdman26@gmail.com>
2024-10-28 01:57:47 +0300

    Added User service. Refactored controller to fit.

--- src/main/java/dev/swe573/whatsthis/controller/UserController.java
+++ src/main/java/dev/swe573/whatsthis/controller/UserController.java
@@ -3,6 +3,7 @@ package dev.swe573.whatsthis.controller;
 import dev.swe573.whatsthis.assembler.UserModelAssembler;
 import dev.swe573.whatsthis.model.User;
 import dev.swe573.whatsthis.repository.UserRepo;
+import dev.swe573.whatsthis.service.UserService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
@@ -14,53 +15,37 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-    private final UserRepo repo;
-    private final UserModelAssembler assembler;
+    private final UserService userService;
 
-    UserController(UserRepo repo, UserModelAssembler assembler) {
-        this.repo = repo;
-        this.assembler = assembler;
+
+    UserController(UserService userService, UserModelAssembler assembler) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
     public CollectionModel<EntityModel<User>> all() {
-        List<EntityModel<User>> users = repo.findAll().stream()
-                .map(assembler::toModel)
-                .collect(Collectors.toList());
-        return CollectionModel.of(users,linkTo(methodOn(UserController.class).all()).withSelfRel());
+        return userService.all();
     }
 
     @PostMapping("/users")
     User newUser(@RequestBody User newUser) {
-        return repo.save(newUser);
+        return userService.newUser(newUser);
     }
 
     @GetMapping("/users/{id}")
     public EntityModel<User> one(@PathVariable Long id) {
-
-        User user = repo.findById(id)
-                .orElseThrow( () -> new UserNotFoundException(id));
-        return assembler.toModel(user);
+        return userService.one(id);
     }
 
     @PutMapping("/users/{id}")
     User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
-        return repo.findById(id)
-                .map(user -> {
-                    user.setUsername(newUser.getUsername());
-                    user.setEmail(newUser.getEmail());
-                    user.setPassword(newUser.getPassword());
-
-                    return repo.save(user);
-                })
-                .orElseGet(() -> {
-                    return repo.save(newUser);
-                });
+        return userService.replaceUser(newUser, id);
     }
 
     @DeleteMapping("/users/{id}")
     void deleteUser(@PathVariable Long id) {
-        repo.deleteById(id);
+        userService.deleteUser(id);
     }
 }
---
+++ src/main/java/dev/swe573/whatsthis/service/UserService.java
@@ -0,0 +1,64 @@
+package dev.swe573.whatsthis.service;
+
+import dev.swe573.whatsthis.assembler.UserModelAssembler;
+import dev.swe573.whatsthis.controller.UserController;
+import dev.swe573.whatsthis.controller.UserNotFoundException;
+import dev.swe573.whatsthis.model.User;
+import dev.swe573.whatsthis.repository.UserRepo;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class UserService {
+
+    UserRepo userRepo;
+    UserModelAssembler assembler;
+
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public CollectionModel<EntityModel<User>> all() {
+        List<EntityModel<User>> users = userRepo.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(users,linkTo(methodOn(UserController.class).all()).withSelfRel());
+    }
+
+    public User newUser(User newUser) {
+        return userRepo.save(newUser);
+    }
+
+    public EntityModel<User> one(@PathVariable Long id) {
+        User user = userRepo.findById(id)
+                .orElseThrow( () -> new UserNotFoundException(id));
+        return assembler.toModel(user);
+    }
+
+    public User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepo.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setPassword(newUser.getPassword());
+
+                    return userRepo.save(user);
+                })
+                .orElseGet(() -> {
+                    return userRepo.save(newUser);
+                });
+    }
+
+    public void deleteUser(@PathVariable Long id) {
+        userRepo.deleteById(id);
+    }
+}
