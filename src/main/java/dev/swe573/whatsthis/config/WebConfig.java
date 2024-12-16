6c49ffa9a35292ffc704d3300e81cd1451643127
Enes Duman <ensdman26@gmail.com>
2024-12-16 23:21:03 +0300

    Static image bug.

--- src/main/java/dev/swe573/whatsthis/config/WebConfig.java
+++ src/main/java/dev/swe573/whatsthis/config/WebConfig.java
@@ -1,6 +1,7 @@
 package dev.swe573.whatsthis.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:uploads/")
                 .setCachePeriod(3600)
                 .resourceChain(true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://34.171.227.193:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 } 
