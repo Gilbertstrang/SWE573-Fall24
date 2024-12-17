1a8010fa7c394a8c4b4d0cf0059275a82e43bce9
Enes Duman <ensdman26@gmail.com>
2024-12-17 05:21:18 +0300

    File upload header issues.

--- src/main/java/dev/swe573/whatsthis/config/WebConfig.java
+++ src/main/java/dev/swe573/whatsthis/config/WebConfig.java
@@ -18,9 +18,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://34.171.227.193:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 } 
